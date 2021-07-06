fe7da578c6002a8c79b5a7eae87248a2517b67b8
Kaalaayaa <kalaya.chatelain@yahoo.fr>
2021-07-06 14:47:57 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import './App.css';
+import './index.css';
+import Title from './Title.js';
+import List from './List.js';
+import Steps from './Steps.js';
+import Footer from './Footer.js';
+import Photo from './Photo';
+
+
 function App() {
-  return <div className="App">Coming soon: an IMPRESSIVE recipe website!!!</div>;
+  return <div className="App">
+    <div className= "Container" className="font-face-gm">
+    <Title />
+    <Photo />
+    <List />
+    <Steps />
+    <Footer />
+    </div>
+    </div>;
 }
 
 export default App;
---
+++ src/List.js
@@ -0,0 +1,21 @@
+import './List.css'
+
+function List() {
+    return ( <div>
+        <ul>
+            <li>1 clove Garlic</li>
+            <li>1 can Chopped Tomatoes</li>
+            <li>1 tbs Mixed Herbs</li>
+            <li>1 tbs Onion Granules</li>
+            <li>1 can Butter Beans</li>
+            <li>100g Artichokes</li>
+            <li>3 tbs Capers</li>
+            <li>1 tsp Sea Salt</li>
+            <li>1 tsp Dried Chilli Flakes</li>
+            <li>2 Servings Cooked Pasta, of your choice</li>
+        </ul>
+        </div>
+    )
+}
+
+export default List;
